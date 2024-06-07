c860f013eb7b72cc305697ce6994be8670a7f931
Silver <theonesilverest@gmail.com>
2024-06-07 14:09:51 +0530

    Volumes for the following have been added: Cone, cuboid, square pyramid, triangular pyramid, triangular prism

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 
+// define the slash command for volume calculations
 const volumeCommand = new SlashCommandBuilder()
   .setName('volume')
   .setDescription('Finds the volume of a shape')
@@ -10,42 +11,136 @@ const volumeCommand = new SlashCommandBuilder()
       .setDescription('Finds the volume of a cube.')
       .addNumberOption((option) =>
         option
-        .setName('s')
-        .setDescription('Input the side length of the cube.')
-        .setRequired(true)
+          .setName('s')
+          .setDescription('Input the side length of the cube.')
+          .setRequired(true)
       )
   )
-
   // subcommand for sphere
   .addSubcommand((subcommand) =>
     subcommand
-    .setName('sphere')
-    .setDescription('Finds the volume of a sphere.')
-    .addNumberOption((option) =>
-      option
-        .setName('r')
-        .setDescription('Input the radius of the sphere.')
-        .setRequired(true)
-    )
+      .setName('sphere')
+      .setDescription('Finds the volume of a sphere.')
+      .addNumberOption((option) =>
+        option
+          .setName('r')
+          .setDescription('Input the radius of the sphere.')
+          .setRequired(true)
+      )
   )
-
   // subcommand for cylinder
   .addSubcommand((subcommand) =>
     subcommand
-    .setName('cylinder')
-    .setDescription('Finds the volume of a cylinder.')
-    .addNumberOption((option) =>
-      option
-        .setName('r')
-        .setDescription('Input the radius of the cylinder.')
-        .setRequired(true)
-    )
-    .addNumberOption((option) =>
-      option
-        .setName('h')
-        .setDescription('Input the height of the cylinder.')
-        .setRequired(true)
-    )
+      .setName('cylinder')
+      .setDescription('Finds the volume of a cylinder.')
+      .addNumberOption((option) =>
+        option
+          .setName('r')
+          .setDescription('Input the radius of the cylinder.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the cylinder.')
+          .setRequired(true)
+      )
+  )
+  // subcommand for cuboid
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('cuboid')
+      .setDescription('Finds the volume of a cuboid.')
+      .addNumberOption((option) =>
+        option
+          .setName('l')
+          .setDescription('Input the length of the cuboid.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('w')
+          .setDescription('Input the width of the cuboid.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the cuboid.')
+          .setRequired(true)
+      )
+  )
+  // subcommand for triangular pyramid
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('triangularpyramid')
+      .setDescription('Finds the volume of a triangular pyramid.')
+      .addNumberOption((option) =>
+        option
+          .setName('b')
+          .setDescription('Input the base length of the triangular pyramid.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the triangular pyramid.')
+          .setRequired(true)
+      )
+  )
+  // subcommand for square pyramid
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('squarepyramid')
+      .setDescription('Finds the volume of a square pyramid.')
+      .addNumberOption((option) =>
+        option
+          .setName('s')
+          .setDescription('Input the base length of the square pyramid.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the square pyramid.')
+          .setRequired(true)
+      )
+  )
+  // subcommand for cone
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('cone')
+      .setDescription('Finds the volume of a cone.')
+      .addNumberOption((option) =>
+        option
+          .setName('r')
+          .setDescription('Input the radius of the cone.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the cone.')
+          .setRequired(true)
+      )
+  )
+  // subcommand for prism
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('triangularprism')
+      .setDescription('Finds the volume of a triangular prism.')
+      .addNumberOption((option) =>
+        option
+          .setName('b')
+          .setDescription('Input the area of the base of the triangular prism.')
+          .setRequired(true)
+      )
+      .addNumberOption((option) =>
+        option
+          .setName('h')
+          .setDescription('Input the height of the triangular prism.')
+          .setRequired(true)
+      )
   );
 
 export default volumeCommand.toJSON();
--- src/index.js
+++ src/index.js
@@ -177,6 +177,42 @@ client.on('interactionCreate', async (interaction) => {
       const V = Math.PI * (r ** 2) * h;
       interaction.reply({ content: `V = π * (${r}^2) * ${h} \nV = ${V}` });
     }
+
+    if (interaction.options.getSubcommand() === 'cuboid') {
+      const l = interaction.options.getNumber('l');
+      const w = interaction.options.getNumber('w');
+      const h = interaction.options.getNumber('h');
+      const V = l * w * h;
+      interaction.reply({ content: `V = ${l} * ${w} * ${h} \nV = ${V}` });
+    }
+
+    if (interaction.options.getSubcommand() === 'triangularpyramid') {
+      const b = interaction.options.getNumber('b');
+      const h = interaction.options.getNumber('h');
+      const V = (1/3) * b * h;
+      interaction.reply({ content: `V = (1/3) * ${b} * ${h} \nV = ${V}` });
+    }
+
+    if (interaction.options.getSubcommand() === 'squarepyramid') {
+      const s = interaction.options.getNumber('s');
+      const h = interaction.options.getNumber('h');
+      const V = (1/3) * (s ** 2) * h;
+      interaction.reply({ content: `V = (1/3) * (${s}^2) * ${h} \nV = ${V}` });
+    }
+
+    if (interaction.options.getSubcommand() === 'cone') {
+      const r = interaction.options.getNumber('r');
+      const h = interaction.options.getNumber('h');
+      const V = (1/3) * Math.PI * (r ** 2) * h;
+      interaction.reply({ content: `V = (1/3) * π * (${r}^2) * ${h} \nV = ${V}` });
+    }
+
+    if (interaction.options.getSubcommand() === 'triangularprism') {
+      const b = interaction.options.getNumber('b');
+      const h = interaction.options.getNumber('h');
+      const V = b * h;
+      interaction.reply({ content: `V = ${b} * ${h} \nV = ${V}` });
+    }
   }
 });
 
